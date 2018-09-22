00d6e0c0fe8c7bf40a5fe7b19b69f782911d394b
Stanciulescu Cecilia Ionela <cecilia.zagrean@gmail.com>
2018-09-22 07:42:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function sayHiToGrandma(string) {
    else if (string.toUpperCase(string) === string) {
     return yesUnswer;
   }
-  else if ("I love you grandma")
+  else if ("I love you, Grandma")
   }
   
